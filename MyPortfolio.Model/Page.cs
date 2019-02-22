80525f9893144e0b109f08545ac53356e69f3fdd
Ayşegül Yılmaz <ayseguulyilmaz@gmail.com>
2019-02-22 11:09:44 +0300

    Page Controller

--- MyPortfolio.Model/Page.cs
+++ MyPortfolio.Model/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace MyPortfolio.Model
 {
     public class Page:BaseEntity
     {
+        [Display(Name ="Başlık")]
         public string Title { get; set; }
 
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Açıklama")]
         public string Description { get; set; }
     }
 }
