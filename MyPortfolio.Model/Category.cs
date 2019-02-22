022e089955515a6d2e783656500ad97ddb5b2c9e
Eser Canik <esercanik@hotmail.com>
2019-02-22 11:03:53 +0300

    Update-PostController

--- MyPortfolio.Model/Category.cs
+++ MyPortfolio.Model/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace MyPortfolio.Model
         {
             Posts = new HashSet<Post>();
         }
+        [Display(Name = "Ad")]
         public string Name { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Post> Posts { get; set; }
