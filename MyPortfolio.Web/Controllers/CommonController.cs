a811118b624cf170bbc07010992d0cb3b9164712
Yigitcan Balcin <ybalcin1@gmail.com>
2019-02-27 14:51:15 +0300

    CommonController,NewsletterPartialView yapıldı

---
+++ MyPortfolio.Web/Controllers/CommonController.cs
@@ -0,0 +1,18 @@
+﻿using MyPortfolio.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Web.Controllers
+{
+    public class CommonController : Controller
+    {
+        // GET: Common
+        public ActionResult Newsletter()
+        {
+            return PartialView("_NewsletterPartial");
+        }
+    }
+}
